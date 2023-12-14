f1da34d7f3c3aa2436c0f9919c45fec38e359ebd
SuperCoder <propscode254@gmail.com>
2023-12-14 19:49:19 +0100

    add app

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 import express from "express";
+import cors from "cors";
+import express from "express";
+import { router as moviesRouter } from "./movies/router.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); //body parser -> header 'Content-Type': 'application/json'
+
+app.use("/api", moviesRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log(console.log("Port is:http://localhost:" + process.env.PORT))
+);
