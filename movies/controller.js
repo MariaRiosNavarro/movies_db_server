edc979c0bffc5de382d98c662a2a5f249a54ab97
SuperCoder <propscode254@gmail.com>
2023-12-14 19:17:48 +0100

    create all controllers

--- movies/controller.js
+++ movies/controller.js
@@ -1,6 +1,9 @@
+import { json } from "express";
 import { dbo } from "../utils/db.js";
 
-export const addMovie = async (req, res) => {
+//!Add One Movie
+
+export const addOneMovie = async (req, res) => {
   try {
     // Extract data from the body of the request
     const movie = {
@@ -20,13 +23,14 @@ export const addMovie = async (req, res) => {
       return res.status(400).json({ error: "Title is necessary." });
     }
 
-    // Save the new movie
+    // Save the new movie in db
     const dbResponse = await dbo.collection("movies").insertOne(movie);
 
-    // Send a successful response
+    //Confirmation back
     if (dbResponse.acknowledged) {
       res.status(201).json({ message: "Movie successfully added ✅" }).end();
     } else {
+      // Handle Error in Database
       res
         .status(500)
         .json({
@@ -37,6 +41,84 @@ export const addMovie = async (req, res) => {
   } catch (error) {
     // Handle errors
     console.error("Error adding movie ❌:", error);
+    res.status(500).json({ error: "Internal Server Error ❌" });
+  }
+};
+
+//!Get All Movie
+
+export const getAllMovies = async (req, res) => {
+  try {
+    //Wait & Save DB data in an Array
+    const dbResponse = await dbo.collection("movies").find().toArray();
+    res
+      .status(200)
+      //Confirmation back & data to frontend
+      .json({ message: "Quotes successfully retrieved ✅", data: dbResponse });
+  } catch (error) {
+    // Handle errors
+    console.error("Error Reading all quotes ❌:", error);
+    res.status(500).json({ error: "Internal Server Error ❌" });
+  }
+};
+
+//!Get One Movie
+
+export const getOneMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    //Wait & Save DB data in an Array
+    const dbResponse = await dbo
+      .collection("movies")
+      .findOne({ _id: new ObjectId(id) });
+    //Confirmation back  & data to frontend
+    res.status(200).json({
+      message: `Movie with id= ${id} sucessfully retrieved ✅`,
+      data: dbResponse,
+    });
+  } catch (error) {
+    // Handle errors
+    console.error(`Error Reading Movie with id= ${id} ❌:`, error);
+    res.status(500).json({ error: "Internal Server Error ❌" });
+  }
+};
+
+//!Delete One Movie
+
+export const deleteOneMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const dbResponse = await dbo
+      .collection("movies")
+      .deleteOne({ _id: new ObjectId(id) });
+    //Confirmation back
+    res
+      .status(200)
+      .json({ message: `Movie with id= ${id} sucessfully deleted ✅` });
+  } catch (error) {
+    // Handle errors
+    console.error(`Error deleting Movie with id= ${id} ❌:`, error);
+    res.status(500).json({ error: "Internal Server Error ❌" });
+  }
+};
+
+//!Edit One Movie
+
+export const editOneMovie = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const newData = req.body;
+    const dbResponse = await dbo
+      .collection("movies")
+      .updateOne({ _id: new ObjectId(id) }, { $set: newData });
+    //Confirmation back
+    res.status(201).json({
+      message: `Movie with id= ${id} sucessfully updated ✅`,
+      data: newData,
+    });
+  } catch (error) {
+    // Handle errors
+    console.error("Error editing Movie ❌:", error);
     res.status(500).json({ error: "Internal Server Error ❌" });
   }
 };
